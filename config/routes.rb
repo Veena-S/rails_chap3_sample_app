fb552aad97f6bfb905b3606e06a91d32cee4a567
Veena-S <veenasivan@gmail.com>
2021-04-14 18:19:17 +0800

    Finish layout and routes for user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   # It’s possible to use a named route other than the default using the as: option.
   # get '/help',  to: 'static_pages#help', as: 'helf'
   get '/help',  to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
