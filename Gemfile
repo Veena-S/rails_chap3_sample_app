ca1a61b2243ced52ab377bc3c6d6c3a34fb950f5
Veena-S <veenasivan@gmail.com>
2021-04-21 12:11:58 +0800

    Created migration to add password_digest for users table and installed the bundle bcrypt

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
+gem 'bcrypt', '3.1.13'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.2.3'
 # Use Puma as the app server
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,18 @@ class User < ApplicationRecord
   # Only those email addresses matching the regular expression are considered to be valid
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-# since the uniqueness validation is case sensitive add an option to nullify it
-# When email is downcased, there is no need of the attribute {case_sensitive: false}
-# since case-sensitive matching works fine if all of the emails are lower-case.
+  # since the uniqueness validation is case sensitive add an option to nullify it
+  # When email is downcased, there is no need of the attribute {case_sensitive: false}
+  # since case-sensitive matching works fine if all of the emails are lower-case.
   validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
+
+  # Rails method "has_secure_password " will add the following functionality:
+  # 1. The ability to save a securely hashed "password_digest" attribute to the database
+  # 2. A pair of virtual attributes ("password" and "password_confirmation"),
+  #  including presence validations upon object creation and a validation requiring that they match
+  # 3. An "authenticate" method that returns the user when the password is correct (and false otherwise)
+  has_secure_password # Rails method
 
 end
