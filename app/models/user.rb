34bddb410c94830f9b1a91bbaa05e17d498f89d7
Veena-S <veenasivan@gmail.com>
2021-04-21 10:56:35 +0800

    Added user name and email length validation, email format validation through regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 # User < ApplicationRecord < ActiveRecord::Base < Object
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 50 }
+  # Only those email addresses matching the regular expression are considered to be valid
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX }
+
 end
