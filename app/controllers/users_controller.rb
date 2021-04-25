f36264716ee5f21e5909ad51b0c9f83b8b349441
Veena-S <veenasivan@gmail.com>
2021-04-25 09:10:22 +0800

    Adding debugger (byebug)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # To get the debug information using byebug gem in the rails server console
+    # debugger
   end
   def new
   end
